1e42ea41686a77233188db45da42c85776b2c95b
adhamnaeim <adhamnaeimmansour@gmail.com>
2025-04-15 18:18:26 +0200

    Create GeneTable component

---
+++ frontend/src/components/GeneTable.tsx
@@ -0,0 +1,48 @@
+import React from 'react';
+
+export type Gene = {
+  ensembl: string;
+  gene_symbol?: string;
+  name?: string;
+  biotype?: string;
+  chromosome: string;
+  start: number;
+  end: number;
+};
+
+type Props = {
+  genes: Gene[];
+};
+
+const GeneTable: React.FC<Props> = ({ genes }) => {
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Ensembl</th>
+          <th>Symbol</th>
+          <th>Name</th>
+          <th>Biotype</th>
+          <th>Chromosome</th>
+          <th>Start</th>
+          <th>End</th>
+        </tr>
+      </thead>
+      <tbody>
+        {genes.map((gene) => (
+          <tr key={gene.ensembl}>
+            <td>{gene.ensembl}</td>
+            <td>{gene.gene_symbol || '-'}</td>
+            <td>{gene.name || '-'}</td>
+            <td>{gene.biotype || '-'}</td>
+            <td>{gene.chromosome}</td>
+            <td>{gene.start}</td>
+            <td>{gene.end}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
+
+export default GeneTable;
